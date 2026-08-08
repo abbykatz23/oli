9f8b404a389814ebe8765aeeaab76d4879290b3c
Abby <abbykatz23@gmail.com>
2026-08-08 15:02:41 -0400

    Fix username in path

--- display_art.py
+++ display_art.py
@@ -21,7 +21,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(message)s",
     handlers=[
-        logging.FileHandler("/home/pi/display_art.log"),
+        logging.FileHandler("/home/oli/display_art.log"),
         logging.StreamHandler(sys.stdout),
     ]
 )
